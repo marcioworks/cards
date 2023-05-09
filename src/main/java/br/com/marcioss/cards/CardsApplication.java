0d40c168fb7f58d442f3f1427c16e5ea1ffbeca2
marcio silva <marcioadsworks@gmail.com>
2023-05-09 19:23:36 -0300

    expose refresh

--- src/main/java/br/com/marcioss/cards/CardsApplication.java
+++ src/main/java/br/com/marcioss/cards/CardsApplication.java
@@ -3,11 +3,13 @@ package br.com.marcioss.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({@ComponentScan("br.com.marcioss.cards.controller")})
 @EnableJpaRepositories("br.com.marcioss.cards.repository")
 @EntityScan("br.com.marcioss.cards.model")
